58f3dd2c7f0d2e4b79cec050a7038c15c5699411
Carolina Bustos <carobustos@MacBook-Pro-de-Carolina.local>
2023-03-24 01:09:35 -0300

    feat: refactored of championship component

--- src/pages/home/components/Championship.tsx
+++ src/pages/home/components/Championship.tsx
@@ -6,21 +6,34 @@ interface ChampionshipProps {
 };
 
 const Championship: FC<ChampionshipProps> = (props: ChampionshipProps) => {
+
+    function renderChampionship() {
+        return (
+            <div>
+                <p> Torneo Imperial </p>
+                <p> Au Córdoba - Juárez Celman, Córdoba </p>
+            </div>
+        )
+    };
+
+    function renderImg() {
+        return (
+            <div>
+                <div>
+                    <img 
+                    src="https://i.ibb.co/rtF9T8k/327313527-589641299678690-1529717078110057676-n.jpg" 
+                    alt='Logo'/>
+                </div>
+            </div>
+        )
+    };
+
     return (
         <div className='Championship__container--wrapper'>
             <CardItem>
                 <div className='Championship__container--item'>
-                    <div>
-                        <p> Torneo Imperial </p>
-                        <p> Au Córdoba - Juárez Celman, Córdoba </p>
-                    </div>
-                    <div>
-                        <div>
-                            <img 
-                            src="https://i.ibb.co/rtF9T8k/327313527-589641299678690-1529717078110057676-n.jpg" 
-                            alt='Logo'/>
-                        </div>
-                    </div>
+                    { renderChampionship() }
+                    { renderImg() }
                 </div>
             </CardItem>
         </div>
