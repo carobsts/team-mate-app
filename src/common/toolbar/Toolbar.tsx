8ca3e247400430c483ff31214da9397b488720ca
Carolina Bustos <carobustos@MacBook-Pro-de-Carolina.local>
2023-03-24 00:34:03 -0300

    feat: created Toolbar component

---
+++ src/common/toolbar/Toolbar.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import './Toolbar.css';
+
+const Toolbar: FC<any> = () => {
+    return (
+        <section className='Toolbar__container--wrapper'>
+            <div>
+                <p></p>
+            </div>
+        </section>
+    )
+};
+
+export default Toolbar;
