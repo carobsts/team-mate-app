e919cb059f236885368c75d2cb448d041f5acb3d
Carolina Bustos <carobustos@MacBook-Pro-de-Carolina.local>
2023-04-11 09:42:30 -0300

    feat: added status code color

---
+++ src/utils/statusColor.enum.ts
@@ -0,0 +1,11 @@
+export enum StatusColor {
+    TO_BE_DONE = 'A realizarse',
+    DONE = 'Realizado',
+    SUSPENDED = 'Suspendido'
+};
+
+export enum StatusCodeColor {
+    TO_BE_DONE = 'gray',
+    DONE = '#478610',
+    SUSPENDED = '#e62e1b'
+};
